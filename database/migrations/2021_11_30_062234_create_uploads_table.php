25aa5479079e8f7a302908191fe0489f8e21b6e0
Andra Septian <andrasept.mail@gmail.com>
2021-11-30 07:58:33 +0000

    sudah pakai template ajihrisproto

---
+++ database/migrations/2021_11_30_062234_create_uploads_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUploadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uploads', function (Blueprint $table) {
+            $table->id();
+            $table->mediumText('doc_name');
+            $table->date('doc_date');
+            $table->date('doc_date_exp');
+            $table->mediumText('doc_note');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uploads');
+    }
+}
---
+++ database/migrations/2021_11_30_074221_create_files_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();          
+            $table->mediumText('doc_name');
+            $table->date('doc_date');
+            $table->date('doc_date_exp');
+            $table->mediumText('doc_note');
+            $table->string('doc_type');
+            $table->string('doc_size');
+            $table->unsignedBigInteger('dept_id');
+            $table->unsignedBigInteger('created_by');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('created_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreign('dept_id')
+                ->references('id')
+                ->on('departments')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
